e600fecf2a5b438a0165f9474c6b3906a9f98522
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-10-11 15:22:35 -0500

    pagina

---
+++ application/views/pages/queremos_conocerte_perfil.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+
+    <title></title>
+    <link rel="stylesheet" href="/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/dist/css/style.css">
+</head>
+
+<body class="bg-gray">
+    <section class="top">
+        <progress class="component-progress" max="100" value="80"></progress>
+        <div class="pleca-roja">
+            <div class="main-wrapper">
+                <figure class="col-12">
+                    <img src="/dist/img/logo.png" alt="">
+                </figure>
+            </div>
+        </div>
+    </section>
+    <section class="main-wrapper">
+        <h1 class="topic text-center mr-bottom-40">¡Queremos conocerte!</h1>
+        <p class="text-center text">Recopilaremos las propiedades ideales para ti. Ayúdanos con tus datos de contacto.</p>
+        <div class="row mr-bottom-40">
+            <div class="col-12 col-sm-6 flex-center">
+                <div class="avatar-space mr-bottom-40">
+                    <img src="" alt="">  
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 item-type-number flex-center mr-bottom-40">
+                <input class="" name="" type="text" placeholder="Nombre" required>
+            </div>
+        </div>
+        
+        <form action="">
+            <div class="row">
+                <div class="col-12">
+                    <p class="text-center text-legal">
+                        <span class="cr-red">*</span>
+                        Compartenos tu número de celular, nos ayudará a estar en contacto ya que recibirás un código de confirmación vía SMS. 
+                    </p>
+                </div>
+                <div class="col-xs-12 col-sm-6 item-type-number">
+                    <span class="cr-red">*</span>    
+                    <input class="" name="" type="number" placeholder="Número de celular (10 dígitos)" required>
+                    <label class="text-legal cr-red hidden" for=""></label>
+                </div>
+                <div class="col-xs-12 col-sm-6 item-type-number">
+                    <span class="cr-red">*</span>
+                    <input class="" name="" type="number" placeholder="Confirmar" required>
+                    <label class="text-legal cr-red hidden" for=""></label>
+                </div>
+                <div class="col-12">
+                    <p class="text-legal">
+                        Tu número movil jamás será usado, compartido o vendido para usos diferentes a los de Plick.
+                    </p>
+                    <div class="check-item">
+                        <input class="styled-checkbox" id="styled-checkbox-terminos" type="checkbox" value="value1">
+                        <label for="styled-checkbox-terminos">He leído y acepto los</label> <a class="link-red" href="">Terminos y condiciones de uso</a>, y <a class="link-red" href="">políticas de privacidad</a>
+                    </div>
+                </div>
+
+            </div>
+        </form>
+    </section>
+    <section class="main-wrapper">
+        <div class="text-center mr-bottom-20">
+            <button class="btn-1">SIGUIENTE</button>
+        </div>
+    </section>
+    <script type="text/javascript" src="/dist/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="/dist/js/bundle.js"></script>
+</body>
+
+</html>
