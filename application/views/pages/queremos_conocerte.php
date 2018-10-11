f2fbfcd36f12a65324ff459bcdd30b2645443975
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-10-11 15:20:43 -0500

    queremos conocerte perfil

--- application/views/pages/queremos_conocerte.php
+++ application/views/pages/queremos_conocerte.php
@@ -92,9 +92,11 @@
        <div>
 
        </div>
-        <div class="text-center mr-bottom-20">
-            <button class="btn-1">SIGUIENTE</button>
-        </div>
+       <form method="post" action="perfil">
+            <div class="text-center mr-bottom-20">
+                <button class="btn-1">SIGUIENTE</button>
+            </div>
+        </form>
     </section>
     <script type="text/javascript" src="/dist/js/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" src="/dist/js/bootstrap.bundle.min.js"></script>
