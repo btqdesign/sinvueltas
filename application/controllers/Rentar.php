1986aa67741091952249842333d72acdb96594d9
Adrian8827 <44100430+Adrian8827@users.noreply.github.com>
2018-10-17 11:13:20 -0500

    Revert "Se agrega el controlador"
    This reverts commit e80f4e2b3526ec8fef1a4815a14a3b565a1fbab1.

--- application/controllers/Rentar.php
+++ application/controllers/Rentar.php
@@ -2,82 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Rentar extends CI_Controller {
-
-	function __construct(){
-		parent:: __construct();
-		$this->load->model('Modelo', 'm');
-		$this->load->helper('form');
-	}
-
 	public function index()
 	{
 		$this->load->view('pages/que_tipo_de_propiedad_necesitas');
 		
     }
-    public function agregarTipo(){
-		$tipo = array(
-			'presupuestomin' => $this->input->post('propiedad'),
-		);
-		$this->m->agregar($tipo);		
-		echo json_encode(array("status" => TRUE));		
-	}	
-
-	public function agregarPresupuesto(){
-		$presupuesto = array(
-			'presupuestomin' => $this->input->post('minimo'),
-			'presupuestomax' => $this->input->post('maximo')
-		);
-		$this->m->agregar($presupuesto);		
-		echo json_encode(array("status" => TRUE));		
-	}
-
-
-
-	public function agregarAmenidades(){
-		$amenidad = array(
-			'presupuestomin' => $this->input->post('habitaciones'),
-		);
-		$this->m->agregar($amenidad);
-		echo json_encode(array("status" => TRUE));		
-	}
-
-	public function agregarLugar(){
-		$lugar = array(
-			'presupuestomin' => $this->input->post('direccion-1'),
-		);
-		$this->m->agregar($lugar);
-		echo json_encode(array("status" => TRUE));		
-	}
-
-	public function agregarCercano(){
-		$cercano = array(
-			'presupuestomin' => $this->input->post('parque'),
-		);
-		$this->m->agregar($cercano);
-		echo json_encode(array("status" => TRUE));		
-	}
-
-	public function agregarUsuario(){
-		$usuario = array(
-			'presupuestomin' => $this->input->post('nombre'),
-		);
-		$this->m->agregar($usuario);
-		echo json_encode(array("status" => TRUE));		
-	}
-
-	public function agregarContacto(){
-		$contacto = array(
-			'presupuestomin' => $this->input->post('nombre'),
-		);		
-		$this->m->agregar($contacto);
-		echo json_encode(array("status" => TRUE));	
-	}
-
-	public function agregarTelefono(){
-		$telefono = array(
-			'presupuestomin' => $this->input->post('codigo'),
-		);		
-		$this->m->agregar($telefono);
-		echo json_encode(array("status" => TRUE));	
-	}
 }
--- application/views/pages/que_tipo_de_propiedad_necesitas.php
+++ application/views/pages/que_tipo_de_propiedad_necesitas.php
@@ -95,124 +95,128 @@
             </div>
         </div>
     </section>
-   <form id="comodoform">        
-        <section class="main-wrapper">
-            <h1 class="topic text-center mr-bottom-30">¿Qué te haría sentir cómodo?</h1>
-                <div class="items width">
-                    <div class="quantity">
-                        <div class="head-icon">
-                            <i class="icon-habitaciones icon-size-35 mr-right-20"></i>
-                            <p class="text">| Habitaciones</p>
-                        </div>
-                        <input type="number" min="1" max="9" step="1" value="1">
+    <section class="main-wrapper">
+        <h1 class="topic text-center mr-bottom-30">¿Qué te haría sentir cómodo?</h1>
+        <form class="tipo">
+            <div class="items width">
+                <div class="quantity">
+                    <div class="head-icon">
+                        <i class="icon-habitaciones icon-size-35 mr-right-20"></i>
+                        <p class="text">| Habitaciones</p>
                     </div>
-                    <div class="quantity">
-                        <div class="head-icon">
-                            <i class="icon-bano icon-size-35 mr-right-20"></i>
-                            <p class="text">| Baño</p>
-                        </div>
-                        <input type="number" min="1" max="9" step="1" value="1">
+                    <input type="number" min="1" max="9" step="1" value="1">
+                </div>
+                <div class="quantity">
+                    <div class="head-icon">
+                        <i class="icon-bano icon-size-35 mr-right-20"></i>
+                        <p class="text">| Baño</p>
                     </div>
-                    <div class="quantity">
-                        <div class="head-icon">
-                            <i class="icon-garage icon-size-35 mr-right-20"></i>
-                            <p class="text">| Estacionamiento</p>
+                    <input type="number" min="1" max="9" step="1" value="1">
+                </div>
+                <div class="quantity">
+                    <div class="head-icon">
+                        <i class="icon-garage icon-size-35 mr-right-20"></i>
+                        <p class="text">| Estacionamiento</p>
+                    </div>
+                    <input type="number" min="1" max="9" step="1" value="1">
+                </div>
+            </div>
+        </form>
+        <p class="text-center text-details">Entre más detalles tengamos de tu parte, mejores resultados</p>
+        <hr class="white">
+    </section>
+    
+    <div class="text-center">
+        <button class="btn btn-secondary btn-collapse" type="button" data-toggle="collapse" data-target="#details" aria-expanded="false" aria-controls="detail">
+            SELECCIONAR MÁS DETALLES
+        </button>
+    </div>
+    <section class="detalles collapse" id="details">
+        <div class="main-wrapper">
+            <form action="">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-1" type="checkbox" value="value1" checked>
+                            <label for="styled-checkbox-1">| Jardin</label>
                         </div>
-                        <input type="number" min="1" max="9" step="1" value="1">
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-2" type="checkbox" value="value2">
+                            <label for="styled-checkbox-2">| Balcones</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-3" type="checkbox" value="value3">
+                            <label for="styled-checkbox-3">| Amueblado</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-4" type="checkbox" value="value4">
+                            <label for="styled-checkbox-4">| Terraza / Roof garden</label>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-5" type="checkbox" value="value5">
+                            <label for="styled-checkbox-5">| Tengo mascota</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-6" type="checkbox" value="value6">
+                            <label for="styled-checkbox-6">| Vigilancia</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-7" type="checkbox" value="value7">
+                            <label for="styled-checkbox-7">| GYM</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-8" type="checkbox" value="value8">
+                            <label for="styled-checkbox-8">| Alberca</label>
+                        </div>                    
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                            <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-9" type="checkbox" value="value9">
+                            <label for="styled-checkbox-9">| Salón de usos multiples</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-10" type="checkbox" value="value10">
+                            <label for="styled-checkbox-10">| Área para niños</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-11" type="checkbox" value="value11">
+                            <label for="styled-checkbox-11">| m2 construidos</label>
+                        </div>
+                        <div class="check-item">
+                            <div class="item-type-number">
+                                <input class="" name="propiedad" type="number" placeholder="MIN">
+                            </div>
+                        </div>
                     </div>
                 </div>
-            <p class="text-center text-details">Entre más detalles tengamos de tu parte, mejores resultados</p>
-            <hr class="white">
-        </section>
-        
-        <div class="text-center">
-            <button class="btn btn-secondary btn-collapse" type="button" data-toggle="collapse" data-target="#details" aria-expanded="false" aria-controls="detail">
-                SELECCIONAR MÁS DETALLES
-            </button>
+                <div class="detalles inline-elements ">
+                    <div class="item-antiguedad">
+                        <input class="styled-checkbox" id="styled-radio-1" name="1" type="radio" value="1" checked>
+                        <label for="styled-radio-1">| NUEVO</label>
+                    </div>
+                    <div class="item-antiguedad">
+                        <input class="styled-checkbox" id="styled-radio-2" name="1" type="radio" value="2">
+                        <label for="styled-radio-2">| 5 a 10 años</label>
+                    </div>
+                    <div class="item-antiguedad">
+                        <input class="styled-checkbox" id="styled-radio-3" name="1" type="radio" value="3">
+                        <label for="styled-radio-3">| 10 a 20 años</label>
+                    </div>
+                    <div class="item-antiguedad">
+                        <input class="styled-checkbox" id="styled-radio-4" name="1" type="radio" value="5">
+                        <label for="styled-radio-4">| + 30 años</label>
+                    </div>
+                </div>
+            </form>
         </div>
-        <section class="detalles collapse" id="details">
-            <div class="main-wrapper">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-4">
-                            <div class="check-item">
-                                <input class="styled-checkbox" id="styled-checkbox-1" type="checkbox" value="value1" checked>
-                                <label for="styled-checkbox-1">| Jardin</label>
-                            </div>
-                            <div class="check-item">
-                                <input class="styled-checkbox" id="styled-checkbox-2" type="checkbox" value="value2">
-                                <label for="styled-checkbox-2">| Balcones</label>
-                            </div>
-                            <div class="check-item">
-                                <input class="styled-checkbox" id="styled-checkbox-3" type="checkbox" value="value3">
-                                <label for="styled-checkbox-3">| Amueblado</label>
-                            </div>
-                            <div class="check-item">
-                                <input class="styled-checkbox" id="styled-checkbox-4" type="checkbox" value="value4">
-                                <label for="styled-checkbox-4">| Terraza / Roof garden</label>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-4">
-                            <div class="check-item">
-                                <input class="styled-checkbox" id="styled-checkbox-5" type="checkbox" value="value5">
-                                <label for="styled-checkbox-5">| Tengo mascota</label>
-                            </div>
-                            <div class="check-item">
-                                <input class="styled-checkbox" id="styled-checkbox-6" type="checkbox" value="value6">
-                                <label for="styled-checkbox-6">| Vigilancia</label>
-                            </div>
-                            <div class="check-item">
-                                <input class="styled-checkbox" id="styled-checkbox-7" type="checkbox" value="value7">
-                                <label for="styled-checkbox-7">| GYM</label>
-                            </div>
-                            <div class="check-item">
-                                <input class="styled-checkbox" id="styled-checkbox-8" type="checkbox" value="value8">
-                                <label for="styled-checkbox-8">| Alberca</label>
-                            </div>                    
-                        </div>
-                        <div class="col-xs-12 col-sm-4">
-                                <div class="check-item">
-                                <input class="styled-checkbox" id="styled-checkbox-9" type="checkbox" value="value9">
-                                <label for="styled-checkbox-9">| Salón de usos multiples</label>
-                            </div>
-                            <div class="check-item">
-                                <input class="styled-checkbox" id="styled-checkbox-10" type="checkbox" value="value10">
-                                <label for="styled-checkbox-10">| Área para niños</label>
-                            </div>
-                            <div class="check-item">
-                                <input class="styled-checkbox" id="styled-checkbox-11" type="checkbox" value="value11">
-                                <label for="styled-checkbox-11">| m2 construidos</label>
-                            </div>
-                            <div class="check-item">
-                                <div class="item-type-number">
-                                    <input class="" name="propiedad" type="number" placeholder="MIN">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="detalles inline-elements ">
-                        <div class="item-antiguedad">
-                            <input class="styled-checkbox" id="styled-radio-1" name="1" type="radio" value="1" checked>
-                            <label for="styled-radio-1">| NUEVO</label>
-                        </div>
-                        <div class="item-antiguedad">
-                            <input class="styled-checkbox" id="styled-radio-2" name="1" type="radio" value="2">
-                            <label for="styled-radio-2">| 5 a 10 años</label>
-                        </div>
-                        <div class="item-antiguedad">
-                            <input class="styled-checkbox" id="styled-radio-3" name="1" type="radio" value="3">
-                            <label for="styled-radio-3">| 10 a 20 años</label>
-                        </div>
-                        <div class="item-antiguedad">
-                            <input class="styled-checkbox" id="styled-radio-4" name="1" type="radio" value="5">
-                            <label for="styled-radio-4">| + 30 años</label>
-                        </div>
-                    </div>
-            </div>
-        </section>
+    </section>
+    <form method="post" id="comodoform">
         <div class="text-center  xs-mrT-40-lg-75 xs-mr-40-lg-75">
-                <button class="btn-1">SIGUIENTE</button>
+            <button class="btn-1">SIGUIENTE</button>
         </div>
-   </form>
+    </form>
        
      </div>
      <div id="gustaria">
