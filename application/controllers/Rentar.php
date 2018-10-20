dd8b2653a0a196496977e47874f20561e72af80f
Adrian8827 <44100430+Adrian8827@users.noreply.github.com>
2018-10-20 14:54:06 -0500

    Update application/controllers/Rentar.php

--- application/controllers/Rentar.php
+++ application/controllers/Rentar.php
@@ -452,6 +452,86 @@ class Rentar extends CI_Controller {
 		        </div>     
         </div>
         ';
+		 }else{
+		 	if ($pro_tipo=="casa") {
+		 		echo '<div class="main-wrapper">            
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-1" name="styled-checkbox-1" type="checkbox" value="10" checked>
+                            <label for="styled-checkbox-1">| Jardin</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-2" name="styled-checkbox-2" type="checkbox" value="5">
+                            <label for="styled-checkbox-2">| Balcones</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-3" name="styled-checkbox-3" type="checkbox" value="3">
+                            <label for="styled-checkbox-3">| Amueblado</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-4" name="styled-checkbox-4" type="checkbox" value="13">
+                            <label for="styled-checkbox-4">| Terraza / Roof garden</label>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-5" name="styled-checkbox-5" type="checkbox" value="11">
+                            <label for="styled-checkbox-5">| Tengo mascota</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-6" name="styled-checkbox-6" type="checkbox" value="14">
+                            <label for="styled-checkbox-6">| Vigilancia</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-7" name="styled-checkbox-7" type="checkbox" value="9">
+                            <label for="styled-checkbox-7">| GYM</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-8" name="styled-checkbox-8" type="checkbox" value="2">
+                            <label for="styled-checkbox-8">| Alberca</label>
+                        </div>                    
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                            <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-9" name="styled-checkbox-9" type="checkbox" value="12">
+                            <label for="styled-checkbox-9">| Salón de usos multiples</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-10" name="styled-checkbox-10" type="checkbox" value="4">
+                            <label for="styled-checkbox-10">| Área para niños</label>
+                        </div>
+                        <div class="check-item">
+                            <input class="styled-checkbox" id="styled-checkbox-11" type="checkbox" value="value11">
+                            <label for="styled-checkbox-11">| m2 construidos</label>
+                        </div>
+                        <div class="check-item">
+                            <div class="item-type-number">
+                                <input class="" name="tamano" type="number" placeholder="MIN">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="detalles inline-elements ">
+                    <div class="item-antiguedad">
+                        <input class="styled-checkbox" id="styled-radio-1" name="antiguedad" type="radio" value="5" checked>
+                        <label for="styled-radio-1">| NUEVO</label>
+                    </div>
+                    <div class="item-antiguedad">
+                        <input class="styled-checkbox" id="styled-radio-2" name="antiguedad" type="radio" value="3">
+                        <label for="styled-radio-2">| 5 a 10 años</label>
+                    </div>
+                    <div class="item-antiguedad">
+                        <input class="styled-checkbox" id="styled-radio-3" name="antiguedad" type="radio" value="1">
+                        <label for="styled-radio-3">| 10 a 20 años</label>
+                    </div>
+                    <div class="item-antiguedad">
+                        <input class="styled-checkbox" id="styled-radio-4" name="antiguedad" type="radio" value="6">
+                        <label for="styled-radio-4">| + 30 años</label>
+                    </div>
+                </div>          
+        </div>';
+		 	}
 		 }
 	}
 }
