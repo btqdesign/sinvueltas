21a02c8f7a3386b97d41ee96e513cbf1b84d3e16
Adrian8827 <44100430+Adrian8827@users.noreply.github.com>
2018-10-20 16:29:21 -0500

    Update application/controllers/Rentar.php

--- application/controllers/Rentar.php
+++ application/controllers/Rentar.php
@@ -455,7 +455,8 @@ class Rentar extends CI_Controller {
 		 }else{
 		 	if ($pro_tipo=="departamento") {
 		 		$vista=$this->load->view('pages/amenidad_renta_depto');
-		 		return $this->set_output($vista); 
+		 		$drawn_view=$this->set_output($vista);
+		 		return ; 
 		 	}
 		 }
 	}
