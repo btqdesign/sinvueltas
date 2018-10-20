3dbbd502e397443689d393206b0ae77d9b84615c
Adrian8827 <44100430+Adrian8827@users.noreply.github.com>
2018-10-20 16:45:23 -0500

    Tipo de amineidades

--- application/views/pages/amenidad_renta_casa.php
+++ application/views/pages/amenidad_renta_casa.php
@@ -11,7 +11,43 @@
 </head>
 
 <body>
-    <div class="main-wrapper">            
+    <form action="" id="comodoform">
+    <section class="main-wrapper">
+        <h1 class="topic text-center mr-bottom-30">¿Qué te haría sentir cómodo?</h1>
+            <div class="items width">
+                <div class="quantity">
+                    <div class="head-icon">
+                        <i class="icon-habitaciones icon-size-35 mr-right-20"></i>
+                        <p class="text">| Habitaciones</p>
+                    </div>
+                    <input type="number" min="1" max="9" step="1" value="1" name="habitaciones">
+                </div>
+                <div class="quantity">
+                    <div class="head-icon">
+                        <i class="icon-bano icon-size-35 mr-right-20"></i>
+                        <p class="text">| Baño</p>
+                    </div>
+                    <input type="number" min="1" max="9" step="1" value="1" name="bano">
+                </div>
+                <div class="quantity">
+                    <div class="head-icon">
+                        <i class="icon-garage icon-size-35 mr-right-20"></i>
+                        <p class="text">| Estacionamiento</p>
+                    </div>
+                    <input type="number" min="1" max="9" step="1" value="1" name="estacionamiento">
+                </div>
+            </div>
+        <p class="text-center text-details">Entre más detalles tengamos de tu parte, mejores resultados</p>
+        <hr class="white">
+    </section>
+    
+    <div class="text-center">
+        <button class="btn btn-secondary btn-collapse" type="button" data-toggle="collapse" data-target="#details" aria-expanded="false" aria-controls="detail">
+            SELECCIONAR MÁS DETALLES
+        </button>
+    </div>
+    <section class="detalles collapse" id="details">
+        <div class="main-wrapper">            
                 <div class="row">
                     <div class="col-xs-12 col-sm-4">
                         <div class="check-item">
@@ -59,16 +95,16 @@
                             <label for="styled-checkbox-10">| Área para niños</label>
                         </div>
                         <div class="check-item">
-                            <input class="" id="styled-checkbox-11" type="hidden" value="value11" >
-                            <!--<label for="styled-checkbox-11">| m2 construidos</label>-->
+                            <input class="styled-checkbox" id="styled-checkbox-11" type="checkbox" value="value11">
+                            <label for="styled-checkbox-11">| m2 construidos</label>
                         </div>
                         <div class="check-item">
                             <div class="item-type-number">
-                                <input class="" name="tamano" type="hidden" placeholder="MIN">
+                                <input class="" name="tamano" type="number" placeholder="MIN">
                             </div>
                         </div>
                     </div>
-                </div>   
+                </div>
                 <div class="detalles inline-elements ">
                     <div class="item-antiguedad">
                         <input class="styled-checkbox" id="styled-radio-1" name="antiguedad" type="radio" value="5" checked>
@@ -86,8 +122,14 @@
                         <input class="styled-checkbox" id="styled-radio-4" name="antiguedad" type="radio" value="6">
                         <label for="styled-radio-4">| + 30 años</label>
                     </div>
-                </div>     
+                </div>          
         </div>
+    </section>
+    <div class="text-center  xs-mrT-40-lg-75 xs-mr-40-lg-75">
+            <button class="btn-1">SIGUIENTE</button>
+    </div>
+     </form>
+       
     <script type="text/javascript" src="/dist/js/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" src="/dist/js/bootstrap.bundle.min.js"></script>
     <script type="text/javascript" src="/dist/js/bundle.js"></script>
