541d3c5505b02b51387c1f1953f3daea88d859fd
Adrian8827 <44100430+Adrian8827@users.noreply.github.com>
2018-10-20 14:29:27 -0500

    Update application/controllers/Rentar.php

--- application/controllers/Rentar.php
+++ application/controllers/Rentar.php
@@ -368,12 +368,11 @@ class Rentar extends CI_Controller {
 	}
 
 	public function tipoAmenidad($identificador){	
-		$this->data['result']   = $this->m->tipoAmenidad($identificador);
-		$pro_id = [];
-		 foreach($this->data['result'] as $key => $val){
-		    $pro_id[] = $val->id;
+		$consulta   = $this->m->tipoAmenidad($identificador);
+		$pro_id;
+		 foreach($consulta as $key => $val){
+		    $pro_id= $val->id;
 		 }
-
 		 print_r($pro_id);
-			}
+	}
 }
