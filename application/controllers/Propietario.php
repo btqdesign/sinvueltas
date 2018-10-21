50cc2658ce2460277f7b5ce0ad83e5d575d86580
Adrian8827 <44100430+Adrian8827@users.noreply.github.com>
2018-10-20 23:26:22 -0500

    Update application/controllers/Propietario.php

--- application/controllers/Propietario.php
+++ application/controllers/Propietario.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Propietario extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('pages/propietario_renta_venta');
+		$this->load->view('pages/propietario_venta_renta');
 		
     }
 }
