992827384b1f51a2c5a446629de1c830b29326f3
Adrian8827 <44100430+Adrian8827@users.noreply.github.com>
2018-10-24 11:47:14 -0500

    coordenas center

--- application/views/pages/que_tipo_de_propiedad_necesitas.php
+++ application/views/pages/que_tipo_de_propiedad_necesitas.php
@@ -738,37 +738,27 @@ label {
 
       function initAutocomplete() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -33.8688, lng: 151.2195},
+          center: {lat: 19.435233, lng: -99.141325},
           zoom: 13,
           mapTypeId: 'roadmap'
         });
-
-        // Create the search box and link it to the UI element.
         var input = document.getElementById('pac-input');
         var searchBox = new google.maps.places.SearchBox(input);
-
-        // Bias the SearchBox results towards current map's viewport.
         map.addListener('bounds_changed', function() {
           searchBox.setBounds(map.getBounds());
         });
 
         var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
         searchBox.addListener('places_changed', function() {
           var places = searchBox.getPlaces();
 
           if (places.length == 0) {
             return;
           }
-
-          // Clear out the old markers.
           markers.forEach(function(marker) {
             marker.setMap(null);
           });
           markers = [];
-
-          // For each place, get the icon, name and location.
           var bounds = new google.maps.LatLngBounds();
           places.forEach(function(place) {
             if (!place.geometry) {
@@ -782,8 +772,6 @@ label {
               anchor: new google.maps.Point(17, 34),
               scaledSize: new google.maps.Size(25, 25)
             };
-
-            // Create a marker for each place.
             markers.push(new google.maps.Marker({
               map: map,
               icon: icon,
@@ -792,7 +780,6 @@ label {
             }));
 
             if (place.geometry.viewport) {
-              // Only geocodes have viewport.
               bounds.union(place.geometry.viewport);
             } else {
               bounds.extend(place.geometry.location);
